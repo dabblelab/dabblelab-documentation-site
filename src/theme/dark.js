c8245f65bfa8213c429f185b9d6c551a6a67067e
Shubham Prakash <shubham.prakash2308@gmail.com>
2020-11-02 15:51:43 +0530

    Add dabblelab  Color Palettes

--- src/theme/dark.js
+++ src/theme/dark.js
@@ -3,14 +3,14 @@ import defaultColors from './colors';
 const colors = {
   ...defaultColors,
 
-  primary: defaultColors.dabblelabOrage,
-  primaryDark: defaultColors.dabblelabOrage,
+  primary: defaultColors.orange,
+  primaryDark: defaultColors.orange,
   font: '#dddddd',
   fontDark: '#8a8a8a',
   background: '#29282A',
   mainBackground: '#1E1E1F',
   border: '#323234',
-  hover: defaultColors.dabblelabOrage,
+  hover: defaultColors.orange,
   shadow: defaultColors.gray + '33',
 };
 
--- src/theme/light.js
+++ src/theme/light.js
@@ -3,14 +3,14 @@ import defaultColors from './colors';
 const colors = {
   ...defaultColors,
 
-  primary: defaultColors.dabblelabOrage,
-  primaryDark: defaultColors.dabblelabOrage,
+  primary: defaultColors.orange,
+  primaryDark: defaultColors.orange,
   font: '#333334',
   fontDark: '#121213',
   background: '#F5F7F9',
   mainBackground: '#fefefe',
   border: '#DBDDDF',
-  hover: defaultColors.dabblelabOrage,
+  hover: defaultColors.orange,
   shadow: defaultColors.gray + '33',
 };
 
